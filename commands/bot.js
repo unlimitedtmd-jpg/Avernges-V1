6e31cbc2e7334531605e06cef7ab2088eb2e444d
unlimitedtmd-jpg <unlimitedtmd@gmail.com>
2026-07-29 13:52:08 +0300

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -3,21 +3,21 @@ module.exports = {
     async execute(socket, msg, number, userConfig, loadUserConfigFromMongo) {
         const sanitized = (number || '').replace(/[^0-9]/g, '');
         const cfg = await loadUserConfigFromMongo(sanitized) || {};
-        const botName = cfg.botName || 'ATLAS-MB彡';
+        const botName = cfg.botName || 'AVERNGES-V1彡';
 
-        const statusText = `─╣ ⚠️ ATLAS • MINI • BOT ⚠️ ╠⁠┈
+        const statusText = `─╣ ⚠️ AVERNGES • MINI • BOT ⚠️ ╠⁠┈
 
 ╭─╣
 ┋  🟢 SYSTEM IS ACTIVE
 ╰─╣⁠
 
 ⚡ STATUS   : ONLINE
-🤖 BOT      : ATLAS-MB彡
-👑 OWNER    : FREDI
+🤖 BOT      : AVERNGES-V1彡
+👑 OWNER    : TIMNASAX
 🧠 MEMORY  : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 
 ━━━━━━━━━━━━━━━━
-🗺️ ATLAS-MINI-BOT 🗺️`;
+🗺️ AVERNGES-MINI-BOT 🗺️`;
 
         await socket.sendMessage(msg.key.remoteJid, {
             text: statusText,
@@ -26,7 +26,7 @@ module.exports = {
                     title: `${botName} is Active`,
                     body: "System: Operational",
                     thumbnailUrl: cfg.logo || 'https://files.catbox.moe/dtmruu.jpeg',
-                    sourceUrl: "https://atlas-mb.vercel.app",
+                    sourceUrl: "https://avernges-v1.vercel.app/",
                     mediaType: 1,
                     renderLargerThumbnail: true
                 }
